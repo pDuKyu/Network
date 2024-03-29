b50da443b28cf2b3ce0d9f068b87b1740409e750
pDuKyu <153824452+pDuKyu@users.noreply.github.com>
2024-03-29 13:47:26 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -399,5 +399,5 @@ st.sidebar.write('')
 st.sidebar.write('')
 st.sidebar.write('')
 
-st.markdown(f"[] ()")
-st.markdown("[서브넷 계산 사이트 링크](https://www.site24x7.com/tools/ipv4-subnetcalculator.html/) ")
+
+st.sidebar.markdown("[서브넷 계산 사이트 링크](https://www.site24x7.com/tools/ipv4-subnetcalculator.html/) ")
