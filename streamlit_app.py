9428976797e8480ca9d3dd66d62d70dad958654f
pDuKyu <153824452+pDuKyu@users.noreply.github.com>
2024-04-25 14:42:40 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1015,6 +1015,120 @@ VTI_Tunnel = {
 }
 
 
+network_commands = {
+    "GRE 명령어": {
+        "명령어": [
+            "(config)#interface tunnel <Tunnel Num>",
+            "(config-if)#tunnel source <SIP or IF>",
+            "(config-if)#tunnel destination <DIP>",
+            "(config-if)#ip address <IP> <Netmask>",
+            "(config-if)#tunnel key <Num>",
+            "(config-if)#keepalive <Num>",
+            "",
+            "show interfaces tunnel <Tunnel Num>",
+            "",
+            "(config)#router ospf 1",
+            "(config-router)#network <T SIP> <Net mask> area 0"
+        ],
+        "설명": [
+            "터널 인터페이스 생성",
+            "터널 Src IP로 사용할 IF 또는 IP 지정",
+            "터널 Dest IP 입력",
+            "터널의 논리적 IF의 IP 지정. 양쪽이 동일한 NET을 가져야 함",
+            "터널의 키 값 지정(양쪽이 동일해야 함)",
+            "Health Check 메세지 초 지정",
+            "",
+            "터널 정보 확인",
+            "",
+            "OSPF 라우터 구성",
+            "터널의 IP로 IGP를 통해 지사 간 Neighbor P2P 연결"
+        ]
+    },
+    "IPsec 명령어": {
+        "IPsec ISAKMP SA 생성": {
+            "명령어": [
+                "(config)#crypto isakmp policy 1",
+                "(config-isakmp)#encryption aes",
+                "(config-isakmp)#hash sha",
+                "(config-isakmp)#authentication pre-share",
+                "(config-isakmp)#group 2",
+                "(config)#crypto isakmp key 0 <MYPASSWORD> address <상대 라우터 퍼블릭 인터페이스 IP>"
+            ],
+            "설명": [
+                "ISAKMP 정책을 설정 (번호 1 사용)",
+                "암호화 알고리즘으로 AES를 선택",
+                "해시 알고리즘으로 SHA를 선택",
+                "사전 공유된 키를 사용하여 인증을 설정",
+                "Diffie-Hellman 그룹 번호를 설정 (그룹 2는 비교적 보안 수준이 높고 효율적인 그룹)",
+                "특정 IP 주소에 대한 ISAKMP 사전 Pre-Shared Key(사전 공유 키)를 설정"
+            ]
+        },
+        "IPsec IPsec SA 생성": {
+            "명령어": [
+                "(config)#crypto ipsec transform-set <MYTRANSFORMSET> esp-aes esp-sha-hmac"
+            ],
+            "설명": [
+                "IPSec 변환 세트를 정의. 이 세트는 데이터를 암호화하는 데 사용되며 AES 암호화 및 SHA 해시를 사용"
+            ]
+        },
+        "Crypto MAP 생성 및 적용": {
+            "명령어": [
+                "(config)#crypto map <CRYPTOMAP> 10 ipsec-isakmp",
+                "(config-crypto-map)#set peer <상대 라우터 퍼블릭 인터페이스 IP>",
+                "(config-crypto-map)#set transform-set <MYTRANSFORMSET>",
+                "(config-crypto-map)#match address 100",
+                "(config-crypto-map)#reverse-route",
+                "(config)#access-list 100 permit ip host <내 호스트IP> host <상대 호스트 IP>",
+                "(config)#interface <Interface>",
+                "(config-if)#crypto map <CRYPTOMAP>"
+            ],
+            "설명": [
+                "IPsec 및 ISAKMP를 위한 crypto 맵 설정. 맵 번호는 10. (crypto map은 하나의 이름만 사용)",
+                "VPN 터널의 대상 피어를 설정",
+                "전에 설정한 IPsec 변환 세트 적용",
+                "암호화 및 인증을 위해 적용할 ACL을 지정. 여기서는 ACL 100",
+                "대상에 대한 Static Route 자동 생성",
+                "ACL 100을 생성하여 IPSec 터널을 통해 통신 할 IP 선정. (Out Bound 방식)",
+                "VPN 설정을 적용할 네트워크 인터페이스 선택.",
+                "인터페이스에 VPN 설정을 적용."
+            ]
+        },
+        "IPsec 기본 명령어": {
+            "명령어": [
+                "통신 시도",
+                "show crypto ipsec sa",
+                "show crypto isakmp sa",
+                "clear crypto isakmp",
+                "clear crypto sa"
+            ],
+            "설명": [
+                "통신 시도로 인해 터널 생성",
+                "IPSec 터널의 암호화 및 복호화 횟수 확인",
+                "ISAKMP 터널 테이블 확인",
+                "ISAKMP 터널 삭제",
+                "IPSec 터널 삭제"
+            ]
+        }
+    },
+    "IPsec Dynamic Crypto Map 명령어": {
+        "IPsec DMAP ISAKMP SA 생성": IPsec_Dynamic_Crypto_MAP_ISAKMP_SA,
+        "IPsec DMAP IPsec SA 생성": IPsec_Dynamic_Crypto_MAP_IPsec_SA,
+        "Dynamic MAP 생성 및 적용": IPsec_Dynamic_Crypto_MAP_IPsec_SA
+    },
+    "GRE over IPsec 명령어": {
+        "GRE over IPsec GRE 생성": GRE_over_IPsec_GRE,
+        "GRE over IPsec ISAKMP SA 생성": GRE_over_IPsec_ISAKMP_SA,
+        "GRE over IPsec IPsec SA 생성": GRE_over_IPsec_IPsec_SA,
+        "GRE over IPsec Crypto MAP 생성 및 적용": GRE_over_IPsec_Crypto_MAP,
+        "IPsec 기본 명령어": GRE_over_IPsec_FINAL
+    },
+    "IPsec VTI 명령어": {
+        "IPsec VTI ISAKMP SA 생성": IPsec_VTI_ISAKMP_SA,
+        "IPsec VTI IPsec SA 생성": IPsec_VTI_IPsec_SA,
+        "IPsec_Profile 생성": IPsec_Profile,
+        "VTI Tunnel 동작": VTI_Tunnel
+    }
+}
 
 
 
@@ -1158,10 +1272,23 @@ elif page == "VPN":
     st.dataframe(selected_df4, width=800)
 
 
+    categories = ["GRE IPsec 명령어", 'IPsec 명령어', 'IPsec Dynamic Crypto Map 명령어', " GRE over IPsec 명령어", "IPsec VTI 명령어"]
+    selected_category = st.selectbox("카테고리 선택", categories)
+    selected_item = st.selectbox("항목 선택", V_tables[selected_category])
 
 
-
-
+    # 주요 명령어 유형 선택
+    selected_command_type = st.sidebar.selectbox("명령어 유형 선택", list(network_commands.keys()))
+    
+    # 선택한 명령어 유형에 따른 데이터 가져오기
+    selected_data = network_commands[selected_command_type]
+    
+    # 데이터프레임 생성
+    df = pd.DataFrame({"명령어": selected_data["명령어"], "설명": selected_data["설명"]})
+    
+    # 선택한 명령어 유형에 따른 명령어 목록 표시
+    st.subheader(f"{selected_command_type} 명령어")
+    st.dataframe(df)
 
 
 
