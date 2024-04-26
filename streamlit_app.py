f8a14a2b1dbd0d90ecd683967cf7ae0655aa9362
pDuKyu <153824452+pDuKyu@users.noreply.github.com>
2024-04-26 12:14:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1283,7 +1283,8 @@ elif page == "Router":
 
 #테이블 시각화
     selected_df2 = r_tables[selected_table2]
-    st.dataframe(selected_df2, width=1200, height=600)
+    num_rows = len(selected_df2)
+    st.dataframe(selected_df2, width=1200, height=num_rows)
     text2 = """
     **라우터**는 네트워크 간의 데이터를 안전하고 효율적으로 전달하는 역할을 하며, 이를 위해 
     
