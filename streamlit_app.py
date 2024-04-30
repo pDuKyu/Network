1f018aaf40c4d7a91373712c2595cc3021fc261b
pDuKyu <153824452+pDuKyu@users.noreply.github.com>
2024-04-30 16:39:23 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1136,7 +1136,7 @@ VPN_LIST = ["GRE 명령어",
             "IPsec VTI 명령어",
            "ASA_IPsec_VPN 명령어"]
 
-network_commands = {
+network_commands = pd.DataFrame({
     "GRE 명령어": {
         "명령어": [
             "(config)#interface tunnel <Tunnel Num>",
@@ -1164,8 +1164,8 @@ network_commands = {
             "OSPF 라우터 구성",
             "터널의 IP로 IGP를 통해 지사 간 Neighbor P2P 연결"
         ]
-    },
-    "IPsec 명령어": {
+    }),
+    "IPsec 명령어": pd.DataFrame({
         "IPsec ISAKMP SA 생성": {
             "명령어": [
                 "(config)#crypto isakmp policy 1",
@@ -1183,7 +1183,7 @@ network_commands = {
                 "Diffie-Hellman 그룹 번호를 설정 (그룹 2는 비교적 보안 수준이 높고 효율적인 그룹)",
                 "특정 IP 주소에 대한 ISAKMP 사전 Pre-Shared Key(사전 공유 키)를 설정"
             ]
-        },
+        }),
         "IPsec IPsec SA 생성": {
             "명령어": [
                 "(config)#crypto ipsec transform-set <MYTRANSFORMSET> esp-aes esp-sha-hmac"
