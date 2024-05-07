d60a49ca9070b002f0cbf8f159538494847bafd1
pDuKyu <153824452+pDuKyu@users.noreply.github.com>
2024-05-07 15:16:45 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1485,7 +1485,8 @@ member_port = {
         "(config-if)# switchport mode trunk",
         "(config-if)# switchport trunk allowed <vlan 1-100>",
         "(config-if)# spanning-tree port guard root",
-        "(config-if)# vpc 10"
+        "(config-if)# vpc 10",
+        ""
     ],
     "설명": [
         "vPC 멤버 포트에 접속하여 설정을 시작",
@@ -1495,7 +1496,8 @@ member_port = {
         "포트 채널을 트렁크 모드로 설정",
         "Member VLAN 설정 (VLAN 1부터 100까지 허용)",
         "포트를 루트 가드로 설정",
-        "포트 채널에 vPC 활성화"
+        "포트 채널에 vPC 활성화",
+        "Peer 장비에도 똑같은 Config 입력"
     ]
 }
 
