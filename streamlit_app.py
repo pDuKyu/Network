0f2d1f1dc900d1befcb7f14147371dcd128d9ac7
pDuKyu <153824452+pDuKyu@users.noreply.github.com>
2024-05-09 17:17:06 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1539,7 +1539,7 @@ vpc_error_commands = {
         "STP/BPDU는 P장비가 주도함. P장비가 Down될 경우 S장비가 STP를 주도하기 위해 주도권을 가져오는데 이때 DownTime이 3초 발생. 이를 방지하기 위해 System MAC을 Bridge ID로 사용하여 두 장비 모두에게 STP 주도권을 주어 DownTime을 없애는 명령어. (두 장비의 STP Priority를 같게 설정해야 함 `spanning-tree vlan <Member VLAN Num> priority <priority>`)",
         "이중화 구성 시 출발 패킷은 가상 MAC 주소를 보고 게이트웨이로 향한 뒤 SVI 인터페이스의 MAC을 src로 달고 내려감. 되돌아 오는 패킷도 마찬가지로 가상 MAC을 향해 보내지며 내려올 때 SVI 인터페이스의 MAC을 달고 내려감. 그러나 패킷을 반송하는 장비가 가상 MAC이 아닌 실제 MAC을 향해 패킷을 보낸다면 패킷 손실이 발생할 수 있음. 예로 만약 P장비의 실제 MAC을 dest로 넣고 패킷을 쏜 뒤 이 패킷이 S장비로 향하게 되면 S장비는 P장비의 MAC정보를 알기 때문에 Peer링크로 이 패킷을 P장비에 전달. 하지만 이는 로컬 처리 룰에 어긋나기 때문에 패킷을 전달하지 않음. 이를 방지하기 위해 P와 S장비가 서로의 실제 MAC 주소를 공유하여 본인이 직접 이 패킷을 로컬 처리 하기 위해 설정하는 명령어. (두 장비는 자신과 상대의 실제 MAC 주소를 G Flag를 달고 테이블에 올림. 실제 Gateway MAC 주소를 공유하여 로컬로 처리할 수 있게 만드는 것)",
         "Peer Link 가 Down되면 Peer 장비들은 상대방의 ARP 정보를 손실하게 됨. Peer Link가 다시 연결됐을 때 ARP 테이블을 채우려면 시간이 걸리게 됨. 이를 방지하기 위해 서로의 ARP 테이블을 공유하여 동일하게 저장하는 명령어.",
-        "Peer Link 다운 시 Orphan Port를 살리는 방법. vPC 도메인에 `dual-active exclude interface-vlan <VLAN Num>` 명령어를 입력하여 Peer Link가 Down되도 VLAN은 UP 상태로 유지.",
+        "Peer Link 다운 시 Orphan Port를 살리는 방법(별도의 인터링크가 필요함). vPC 도메인에 `dual-active exclude interface-vlan <VLAN Num>` 명령어를 입력하여 Peer Link가 Down되도 VLAN은 UP 상태로 유지.",
         "Peer Link 다운 시 Orphan Port를 죽이는 방법. Orphan Port 인터페이스에 `vpc orphan-ports suspend` 명령어를 입력하여 Peer Link가 다운될 시 Orphan Port도 함께 Down시킴."
     ]
 }
